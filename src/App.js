5e2d9c5288f0d2644feac68bb75c608a4b8e8318
Caner Dagli <cdagli@gmail.com>
2017-11-17 21:25:03 +0300

    some text changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header>
+          <h1>Welcome to DuruApp</h1>
         </header>
       </div>
     );
